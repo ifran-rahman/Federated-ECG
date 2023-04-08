2804c58170a275eaaa17cdf0533557c135edaadc
Ifran Rahman Nijhum <52383588+ifran-rahman@users.noreply.github.com>
2023-04-09 02:05:01 +0600

    delete file

--- ECG_Processing_API/data_process/pre_process.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def series_to_string(ecg_series):
-    return ecg_series.to_string(index=False)
-
-
-def string_to_series(ecg_string):
-    return pd.Series(ecg_string.split('\n'))
