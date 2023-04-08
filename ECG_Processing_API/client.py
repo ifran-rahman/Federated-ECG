4937e02648c3fa640648439f98f1b769a57f274e
Ifran Rahman Nijhum <52383588+ifran-rahman@users.noreply.github.com>
2023-04-09 01:14:49 +0600

    clean code client.py

--- ECG_Processing_API/client.py
+++ ECG_Processing_API/client.py
@@ -1,4 +1,4 @@
-## This script deals with posting and getting data from the API(s)
+## This script deals with posting and getting data from the API
 
 import requests
 from data_process.dataProcessor import signal_to_beats
@@ -25,10 +25,7 @@ def get_signal():
 
 # get signal
 signal = get_signal()
-
 signal_data = signal.values.tolist()
-
-# print(signal_str)
 
 # POST ECG signal to ECG-Process API
 url = "http://127.0.0.1:8000/signals/"
@@ -39,28 +36,24 @@ data = {
    "is_verified": 0
 }
 
-upload_signal = requests.post(url, json = data)
+# POST signal
+API_response = requests.post(url, json = data)
 
-
-print(upload_signal.text)
-
-# get processed heartbeats
-
-beats_json = 0 #later add codes of json parsed beats json
-
-json_data = upload_signal.json()
+# GET heartbeats
+json_data = API_response.json()
 beats_json = json_data['beats']
 
-# get beats and convert beats_json to list
+# GET beats and convert beats_json to list
 beats = beats_str_to_list(beats_json)
 
-# get annotation from api
+# get annotation for the current signal
+# we assume the annotation for the current signal is 1 (Abnormal)
 annotation = 1
 
 # choose dataset path according to annotation
 if annotation == 0:
     path = "E:/Work/TnR Lab/FED-MAIN/Federated-ECG/ECG Classification/datasets/ptbdb_normal.csv"
-else:
+else: 
     path = "E:/Work/TnR Lab/FED-MAIN/Federated-ECG/ECG Classification/datasets/ptbdb_abnormal.csv"
 
 # Create a DataFrame from the list of NumPy arrays
@@ -69,6 +62,7 @@ new_beats = pd.DataFrame(beats[1: (len(beats)-1)])
 
 # Load the existing CSV file
 existing_dataset = pd.read_csv(path, header=None)
+
 # Add the DataFrame to the existing DataFrame
 new_df = pd.concat([existing_dataset, new_beats], axis=0)
 
