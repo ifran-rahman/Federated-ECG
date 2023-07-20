94847065c6e9c40318ebcb26bbf5ab897a7e2824
ifran-rahman <ifranrahmannijhum@gmail.com>
2023-07-20 11:03:18 -0400

    comment out lines for testing

--- ECG_Processing_API/client.py
+++ ECG_Processing_API/client.py
@@ -35,25 +35,26 @@ beats_json = json_data['beats']
 # GET beats and convert beats_json to list
 beats = beats_str_to_list(beats_json)
 
-# get annotation for the current signal
-# we assume the annotation for the current signal is 1 (Abnormal)
-annotation = 1
+print(beats)
+# # get annotation for the current signal
+# # we assume the annotation for the current signal is 1 (Abnormal)
+# annotation = 1
 
-# choose dataset path according to annotation
-if annotation == 0:
-    path = "E:/Work/TnR Lab/FED-MAIN/Federated-ECG/ECG Classification/datasets/ptbdb_normal.csv"
-else: 
-    path = "E:/Work/TnR Lab/FED-MAIN/Federated-ECG/ECG Classification/datasets/ptbdb_abnormal.csv"
+# # choose dataset path according to annotation
+# if annotation == 0:
+#     path = "E:/Work/TnR Lab/FED-MAIN/Federated-ECG/ECG Classification/datasets/ptbdb_normal.csv"
+# else: 
+#     path = "E:/Work/TnR Lab/FED-MAIN/Federated-ECG/ECG Classification/datasets/ptbdb_abnormal.csv"
 
-# Create a DataFrame from the list of NumPy arrays
-# remove the first and the last beat as they tend to remain incomplete
-new_beats = pd.DataFrame(beats[1: (len(beats)-1)])
+# # Create a DataFrame from the list of NumPy arrays
+# # remove the first and the last beat as they tend to remain incomplete
+# new_beats = pd.DataFrame(beats[1: (len(beats)-1)])
 
-# Load the existing CSV file
-existing_dataset = pd.read_csv(path, header=None)
+# # Load the existing CSV file
+# existing_dataset = pd.read_csv(path, header=None)
 
-# Add the DataFrame to the existing DataFrame
-new_df = pd.concat([existing_dataset, new_beats], axis=0)
+# # Add the DataFrame to the existing DataFrame
+# new_df = pd.concat([existing_dataset, new_beats], axis=0)
 
-# Write the new DataFrame to the CSV file
-new_df.to_csv(path, index=False)
+# # Write the new DataFrame to the CSV file
+# new_df.to_csv(path, index=False)
